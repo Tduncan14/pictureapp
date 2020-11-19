d3463e3d165e423516b6e177a4cf1b497c9a1e42
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-11-19 01:17:03 -0600

    keep moving forward

--- server/server.js
+++ server/server.js
@@ -1,2 +1,36 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const dot = require('dotenv')
+
+dot.config();
+
+const app = express()
+
+
+
+mongoose.connect({})
+.then(() => {
+
+    console.log('the database is not connected')
+})
+
+app.use(cors())
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(morgan())
+
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(() =>{
+
+    console.log(`now listening on ${PORT}`)
+
+
+})
