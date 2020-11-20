8763a062d4c5736874cdcefc1745840442503bf2
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-11-20 05:03:09 -0600

    keep moving forward

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,13 +1,38 @@
+const PostMessage = require('../models');
 
 
-  const getPosts = (req,res) => {
+  const getPosts =  aysnc(req,res)  => {
 
-    res.send('This Works')
-}
+      try{
+
+         const PostMessages = await PostMessage.find();
+         
+          console.log(postMessage);
+
+
+          res.status(200).json(postMessages)
+          
+
+      }
+
+      catch(err){
+
+        res.status(404).json('messages not found')
+
+      }
+
+
+
+
+  }
+
+ const createPost = (req,res) =>{
+
+    res.send('create Post')
+ }
 
 
 
 
 
-
-module.exports = {getPosts,}
+module.exports = {getPosts,createPost}
