e75d165e06ef28f54e744379e7684dd5d2752a28
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-11-21 04:36:04 -0600

    keep moving forward

---
+++ picture/src/components/Posts/Post/Post.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+const Post = () => {
+
+
+   return(
+       <div>
+
+      <h1> Just the post </h1>
+
+
+
+
+       </div>
+   )
+
+
+
+
+
+}
+
+
+
+export default Post
